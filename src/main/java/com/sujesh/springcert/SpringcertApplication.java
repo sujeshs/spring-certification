a154ae543b6f016b77a5d1d12df9e88fb5b5ada9
santosterock <santosterock@gmail.com>
2019-09-16 19:50:17 -0700

    initial commit

---
+++ src/main/java/com/sujesh/springcert/SpringcertApplication.java
@@ -0,0 +1,13 @@
+package com.sujesh.springcert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcertApplication.class, args);
+	}
+
+}
